7cef4dca6843e07683bca4b775b4c552eefe441f
Eder Oliveira <ederoliveira152@gmail.com>
2025-03-14 10:19:16 -0300

    initialized

---
+++ src/test/java/br/com/ederoliv/goeat_api/GoeatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ederoliv.goeat_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoeatApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
